22b7a5afe40e36af3ec06dc3a7d6761161f32a17
Jeroen el Hahaoui <j.el.hahaoui@micro-it.nl>
2023-06-05 22:38:59 +0200

    Added StoreConnector

---
+++ src/StoreConnector.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Microit\StoreBase;
+
+use Microit\StoreBase\Collections\CategoryCollection;
+use Microit\StoreBase\Collections\ProductCollection;
+use Microit\StoreBase\Models\Category;
+use Microit\StoreBase\Models\Product;
+
+abstract class StoreConnector
+{
+    abstract public function getCategories(): CategoryCollection;
+
+    abstract public function getProductsOfCategory(Category $category): ProductCollection;
+
+    abstract public function getProductsByName(string $name): ProductCollection;
+
+    abstract public function getProductById(int|string $id): Product;
+}
